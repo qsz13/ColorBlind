32ff04ba83d5bb69cf976426fa1ab231d1207a0d
qsz13 <qsz1328@gmail.com>
2015-04-02 17:30:41 +0800

    init

---
+++ ColorBlind/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+
+namespace ColorBlind
+{
+
+  
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenu contextMenu;
+
+        private System.Windows.Forms.MenuItem protanopeMenuItem;    //红色盲
+        private System.Windows.Forms.MenuItem deuteranopeMenuItem;  //绿色盲
+        private System.Windows.Forms.MenuItem tritanopeMenuItem;   //蓝色盲
+
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            InitializeContextMenu();
+            InitialTray();
+        }
+
+        private void InitializeContextMenu()
+        {
+            this.contextMenu = new System.Windows.Forms.ContextMenu();
+            contextMenu.MenuItems.Add(
+                new System.Windows.Forms.MenuItem("退出", new System.EventHandler(Exit_Click)));
+            protanopeMenuItem = new System.Windows.Forms.MenuItem("红色盲", new System.EventHandler(Type_Click));
+            deuteranopeMenuItem = new System.Windows.Forms.MenuItem("绿色盲", new System.EventHandler(Type_Click));
+            tritanopeMenuItem = new System.Windows.Forms.MenuItem("蓝色盲", new System.EventHandler(Type_Click));
+            protanopeMenuItem.RadioCheck = true;
+            deuteranopeMenuItem.RadioCheck = true;
+            tritanopeMenuItem.RadioCheck = true;
+            contextMenu.MenuItems.Add(protanopeMenuItem);
+            contextMenu.MenuItems.Add(deuteranopeMenuItem);
+            contextMenu.MenuItems.Add(tritanopeMenuItem);
+   
+        }
+
+        protected void Type_Click(Object sender, System.EventArgs e)
+        {
+
+
+            if (sender == protanopeMenuItem)
+            {
+                protanopeMenuItem.Checked = true;
+                deuteranopeMenuItem.Checked = false;
+                tritanopeMenuItem.Checked = false;
+            }
+
+            else if(sender == deuteranopeMenuItem)
+            {
+                protanopeMenuItem.Checked = false;
+                deuteranopeMenuItem.Checked = true;
+                tritanopeMenuItem.Checked = false;
+            }
+
+            else if(sender == tritanopeMenuItem)
+            {
+
+                protanopeMenuItem.Checked = false;
+                deuteranopeMenuItem.Checked = false;
+                tritanopeMenuItem.Checked = true;
+            }
+
+
+
+
+
+        }
+
+
+        protected void Exit_Click(Object sender, System.EventArgs e)
+        {
+            Close();
+        }
+        private void InitialTray()
+        {
+            notifyIcon = new System.Windows.Forms.NotifyIcon();
+            notifyIcon.Icon = ColorBlind.Properties.Resources.MainIcon;
+            notifyIcon.Visible = true;
+            notifyIcon.ContextMenu = contextMenu;
+            notifyIcon.Click += new System.EventHandler(this.notifyIcon_Click);
+        }
+
+        private void notifyIcon_Click(object Sender, EventArgs e)
+        {
+           
+        }
+
+
+    }
+}
